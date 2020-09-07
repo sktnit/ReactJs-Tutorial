6913257d268ced410b5e730fa9dc603fb663a174
shaileshnit <sktmanit@gmail.com>
2020-09-07 11:19:00 +0530

    Empty Tag in React

---
+++ tut2/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App()
+{
+  return (
+
+    //if react js version >16.x.x empty tag is allowed
+    <>
+      <h1> Shailesh</h1>
+      <p>
+        Welcome to Node JS tutorial!
+      </p>
+    </>
+  );
+}
+export default App;
