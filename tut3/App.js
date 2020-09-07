184e1aeea314736be41c718106ba9b93d364f10d
shaileshnit <sktmanit@gmail.com>
2020-09-07 11:29:25 +0530

    Return an array of elements in react

---
+++ tut3/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App()
+{
+  return (
+
+    //we can return the array of elements also.
+    [
+      <h1> Shailesh</h1>,
+      <p>
+        Welcome to Node JS tutorial!
+      </p>,
+      <span> Return an array for displaying an array of elements </span>
+    ]
+  );
+}
+export default App;
