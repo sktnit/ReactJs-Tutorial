875cc833a4947a4bedc8fac863a9eca450f1c5c3
shaileshnit <sktmanit@gmail.com>
2020-09-07 11:03:40 +0530

    First Program

---
+++ tut1/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App()
+{
+  return (
+    <div className="App">
+      <h1> First Program: React uses component based approach and uses JSX-Javascript XML or also known as advanced javascript </h1>
+    </div>
+  );
+}
+
+export default App;
