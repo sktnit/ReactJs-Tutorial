2982faa87c871aed51cfabfbfcb652103b83d4a0
shaileshnit <sktmanit@gmail.com>
2020-09-07 11:47:02 +0530

    React.Fragment in reactjs

---
+++ tut4/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App()
+{
+  return (
+
+    //React.Fragment group a list of children 
+    //without adding extra nodes to the DOM.
+    <React.Fragment>
+      <h1> Shailesh</h1>
+      <p>
+        Welcome to Node JS tutorial!
+      </p>
+      <span> React Fragment  </span>
+    </React.Fragment>
+  );
+}
+export default App;
