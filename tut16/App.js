07da0b734970c770874839a9964b1b8a11545523
shaileshnit <sktmanit@gmail.com>
2020-09-10 02:37:55 +0530

    preventdefault and form  in react

---
+++ tut16/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+// events in react
+function App()
+{
+  const [name, setName] = useState("");
+  const [fullName, setFullName] = useState('');
+
+  const inpEvent = (event) =>
+  {
+    console.log(event.target.value);
+    setName(event.target.value);
+  };
+
+  const onSubmits = (event) =>
+  {
+    event.preventDefault();
+    setFullName(name);
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmits}>
+        <div>
+          <h1>Hello {fullName}</h1>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            defaultValue=""
+            onChange={inpEvent}
+            value={name}
+          />
+          <button type="submit">Click Me</button>
+        </div>
+      </form>
+    </div>);
+}
+
+export default App;
